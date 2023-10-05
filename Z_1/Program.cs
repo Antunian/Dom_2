e5436a08c525cc9b5a50dbf92253704401638352
unknown <antunian007@gmail.com>
2023-10-05 17:52:12 +0300

    z-1

---
+++ Z_1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.Write("Введите число A: ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите число B: ");
+int b = int.Parse(Console.ReadLine());
+double c = Math.Pow((a), b);
+
+Console.Write($"Число A в натуральной степени B равно {c}");
